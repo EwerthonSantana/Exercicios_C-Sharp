a8d629261473fdcebcc7d14f60bed8d1bdcc36a3
Ewerthon Santana <ewerthon32santana@gmail.com>
2022-05-26 14:31:09 -0300

    adicionado o projeto Conjuntos

---
+++ Conjuntos/Conjuntos/Entities/Student.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Conjuntos.Entities {
+    internal class Student {
+        public int Id { get; set; }
+
+        public Student(int id) {
+            Id = id;
+        }
+
+        public override int GetHashCode() {
+            return Id.GetHashCode();
+        }
+
+        public override bool Equals(object? obj) {
+            if (!(obj is Student))
+                return false;
+
+            Student other = obj as Student;
+            return Id.Equals(other.Id);
+        }
+
+    }
+}
---
+++ Conjuntos/Conjuntos/Program.cs
@@ -0,0 +1,26 @@
+﻿using Conjuntos.Entities;
+
+HashSet<Student> studants = new HashSet<Student>();
+
+Console.Write("How many students for course A? ");
+int amountStudentsA = int.Parse(Console.ReadLine());
+for (int i = 0; i < amountStudentsA; i++) {
+    int idStudant = int.Parse(Console.ReadLine());
+    studants.Add(new(idStudant));
+}
+
+Console.Write("How many students for course B? ");
+int amountStudentsB = int.Parse(Console.ReadLine());
+for (int i = 0; i < amountStudentsB; i++) {
+    int idStudant = int.Parse(Console.ReadLine());
+    studants.Add(new(idStudant));
+}
+
+Console.Write("How many students for course C? ");
+int amountStudentsC = int.Parse(Console.ReadLine());
+for (int i = 0; i < amountStudentsC; i++) {
+    int idStudant = int.Parse(Console.ReadLine());
+    studants.Add(new(idStudant));
+}
+
+Console.WriteLine("Total students: " + studants.Count);
